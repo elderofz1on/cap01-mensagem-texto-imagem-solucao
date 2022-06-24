5d5348482d3b9f4eaf5a2044abcc1e46fa400dce
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 16:31:14 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.py
@@ -0,0 +1,7 @@
+basic.show_string("alo mundo!!!")
+basic.show_string("HACKIDS")
+
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.show_string("BRASIL")
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.showString("micro:bit")
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showString("BRASIL")
+})
